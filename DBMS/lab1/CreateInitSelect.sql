989743e06802826052d18728a22107b8e0506591
PC\Andrey <minaldandrey@gmail.com>
2018-10-07 02:02:12 +0300

    DBMS - Implementing second lab script

--- DBMS/lab1/CreateInitSelect.sql
+++ DBMS/lab1/CreateInitSelect.sql
@@ -24,16 +24,14 @@ CREATE TABLE Tournaments (
 	StartDate DATE NOT NULL CHECK (StartDate > TO_DATE('01-01-1920', 'dd-mm-yyyy')), 
 	EndDate DATE NOT NULL CHECK (EndDate > TO_DATE('01-01-1920', 'dd-mm-yyyy')), 
 	RegistrationFee NUMBER, 
-	TournamentSystem VARCHAR2(100) NOT NULL CHECK(TournamentSystem IN('One round-robin', 'Double round-robin', 'Swiss', 'Olympic', 'Match')), 
-	PrizeForFirstPlace NUMBER, 
-	PrizeForSecondPlace NUMBER, 
-	PrizeForThirdPlace NUMBER 
+	TournamentSystem VARCHAR2(100) NOT NULL CHECK(TournamentSystem IN('One round-robin', 'Double round-robin', 'Swiss', 'Olympic', 'Match'))
 ); 
 
 CREATE TABLE TournamentPlayers ( 
 	Id NUMBER NOT NULL PRIMARY KEY, 
 	TournamentId NUMBER NOT NULL, 
 	PlayerId NUMBER NOT NULL, 
+	Prize NUMBER,
 	CONSTRAINT FK_TournamentPlayers_Tournaments FOREIGN KEY (TournamentId) REFERENCES Tournaments(Id), 
 	CONSTRAINT FK_TournamentPlayers_Players FOREIGN KEY (PlayerId) REFERENCES Players(Id) 
 ); 
@@ -62,30 +60,59 @@ INSERT INTO Players VALUES (6, 'Aronian', 'Levon', 'Grigorievich', 2780, TO_DATE
 INSERT INTO Players VALUES (7, 'Giri', 'Anish', 'Kumar', 2780, TO_DATE('28-06-1994', 'dd-mm-yyyy'), TO_DATE('01-07-2000', 'dd-mm-yyyy'), 'true', 0); 
 INSERT INTO Players VALUES (8, 'Kramnik', 'Vladimir', 'Borisovich', 2779, TO_DATE('25-06-1975', 'dd-mm-yyyy'), TO_DATE('01-08-2000', 'dd-mm-yyyy'), 'true', 20); 
 INSERT INTO Players VALUES (9, 'So', 'Wesley', 'Barbasa', 2776, TO_DATE('09-11-1993', 'dd-mm-yyyy'), TO_DATE('01-09-2000', 'dd-mm-yyyy'), 'true', 0); 
-INSERT INTO Players VALUES (10, 'Anand', 'Viswanathan', null, 2771, TO_DATE('11-12-1969', 'dd-mm-yyyy'), TO_DATE('01-10-2000', 'dd-mm-yyyy'), 'true', 0); 
+INSERT INTO Players VALUES (10, 'Anand', 'Viswanathan', null, 2771, TO_DATE('11-12-1969', 'dd-mm-yyyy'), TO_DATE('01-10-2000', 'dd-mm-yyyy'), 'false', 0);
+INSERT INTO Players VALUES (11, 'Grischuk', 'Alexander', 'Igorevich', 2769, TO_DATE('31-10-1983', 'dd-mm-yyyy'), TO_DATE('01-11-2000', 'dd-mm-yyyy'), 'true', 0); 
+INSERT INTO Players VALUES (12, 'Yu', 'Yangyi', null, 2765, TO_DATE('08-06-1994', 'dd-mm-yyyy'), TO_DATE('01-12-2000', 'dd-mm-yyyy'), 'false', 0); 
+INSERT INTO Players VALUES (13, 'Nakamura', 'Hikaru', null, 2763, TO_DATE('09-12-1987', 'dd-mm-yyyy'), TO_DATE('01-01-2001', 'dd-mm-yyyy'), 'true', 0); 
+INSERT INTO Players VALUES (14, 'Karjakin', 'Sergey', 'Aleksandrovich', 2760, TO_DATE('12-01-1990', 'dd-mm-yyyy'), TO_DATE('01-02-2001', 'dd-mm-yyyy'), 'true', 0); 
+INSERT INTO Players VALUES (15, 'Nepomniachtchi', 'Ian', 'Aleksandrovich', 2759, TO_DATE('14-07-1990', 'dd-mm-yyyy'), TO_DATE('01-03-2001', 'dd-mm-yyyy'), 'true', 0); 
+INSERT INTO Players VALUES (16, 'Svidler', 'Peter', 'Veniaminovich', 2756, TO_DATE('17-06-1976', 'dd-mm-yyyy'), TO_DATE('01-04-2001', 'dd-mm-yyyy'), 'true', 0); 
 
-INSERT INTO Tournaments VALUES (1, 'Candidates Tournament 2018', TO_DATE('10-03-2018', 'dd-mm-yyyy'), TO_DATE('28-03-2018', 'dd-mm-yyyy'), 0, 'Double round-robin', 95000, 88000, 75000); 
-INSERT INTO Tournaments VALUES (2, 'Chess World Cup 2017', TO_DATE('02-09-2017', 'dd-mm-yyyy'), TO_DATE('27-09-2017', 'dd-mm-yyyy'), 1000, 'Olympic', 120000, 80000, 50000); 
+INSERT INTO Tournaments VALUES (1, 'Candidates Tournament 2018', TO_DATE('10-03-2018', 'dd-mm-yyyy'), TO_DATE('28-03-2018', 'dd-mm-yyyy'), 0, 'Double round-robin'); 
+INSERT INTO Tournaments VALUES (2, 'Chess World Cup 2017', TO_DATE('02-09-2017', 'dd-mm-yyyy'), TO_DATE('27-09-2017', 'dd-mm-yyyy'), 1000, 'Olympic'); 
 
-INSERT INTO TournamentPlayers VALUES (1, 1, 2); 
-INSERT INTO TournamentPlayers VALUES (2, 1, 3); 
-INSERT INTO TournamentPlayers VALUES (3, 1, 4); 
-INSERT INTO TournamentPlayers VALUES (4, 1, 6); 
-INSERT INTO TournamentPlayers VALUES (5, 1, 8); 
-INSERT INTO TournamentPlayers VALUES (6, 1, 9); 
-INSERT INTO TournamentPlayers VALUES (7, 2, 1); 
-INSERT INTO TournamentPlayers VALUES (8, 2, 2); 
-INSERT INTO TournamentPlayers VALUES (9, 2, 3); 
-INSERT INTO TournamentPlayers VALUES (10, 2, 4); 
-INSERT INTO TournamentPlayers VALUES (11, 2, 5); 
-INSERT INTO TournamentPlayers VALUES (12, 2, 6); 
-INSERT INTO TournamentPlayers VALUES (13, 2, 7); 
-INSERT INTO TournamentPlayers VALUES (14, 2, 8); 
-INSERT INTO TournamentPlayers VALUES (15, 2, 9); 
-INSERT INTO TournamentPlayers VALUES (16, 2, 10); 
+INSERT INTO TournamentPlayers VALUES (1, 1, 2, 95000); 
+INSERT INTO TournamentPlayers VALUES (2, 1, 3, 88000); 
+INSERT INTO TournamentPlayers VALUES (3, 1, 4, 55000); 
+INSERT INTO TournamentPlayers VALUES (4, 1, 6, 17000); 
+INSERT INTO TournamentPlayers VALUES (5, 1, 8, 40000); 
+INSERT INTO TournamentPlayers VALUES (6, 1, 9, 22000); 
+INSERT INTO TournamentPlayers VALUES (7, 1, 11, 28000);
+INSERT INTO TournamentPlayers VALUES (8, 1, 14, 75000);
+INSERT INTO TournamentPlayers VALUES (9, 2, 1, 16000); 
+INSERT INTO TournamentPlayers VALUES (10, 2, 2, 16000); 
+INSERT INTO TournamentPlayers VALUES (11, 2, 3, 10000); 
+INSERT INTO TournamentPlayers VALUES (12, 2, 4, 80000); 
+INSERT INTO TournamentPlayers VALUES (13, 2, 5, 50000); 
+INSERT INTO TournamentPlayers VALUES (14, 2, 6, 120000); 
+INSERT INTO TournamentPlayers VALUES (15, 2, 7, 25000); 
+INSERT INTO TournamentPlayers VALUES (16, 2, 8, 16000); 
+INSERT INTO TournamentPlayers VALUES (17, 2, 9, 50000); 
+INSERT INTO TournamentPlayers VALUES (18, 2, 10, 10000); 
+INSERT INTO TournamentPlayers VALUES (19, 2, 11, 25000);
+INSERT INTO TournamentPlayers VALUES (20, 2, 12, 10000);
+INSERT INTO TournamentPlayers VALUES (21, 2, 13, 16000); 
+INSERT INTO TournamentPlayers VALUES (22, 2, 14, 10000); 
+INSERT INTO TournamentPlayers VALUES (23, 2, 15, 16000); 
+INSERT INTO TournamentPlayers VALUES (24, 2, 16, 35000);  
 
-INSERT INTO Games VALUES (1, 1, TO_DATE('10-03-2018', 'dd-mm-yyyy'), 6, 4, '0.5-0.5', 'Catalan Opening', 62); 
-INSERT INTO Games VALUES (2, 1, TO_DATE('10-03-2018', 'dd-mm-yyyy'), 2, 9, '1-0', 'Four Knights Game', 57); 
-INSERT INTO Games VALUES (3, 1, TO_DATE('12-03-2018', 'dd-mm-yyyy'), 2, 3, '0.5-0.5', 'Caro–Kann Defense', 32); 
-INSERT INTO Games VALUES (4, 1, TO_DATE('15-03-2018', 'dd-mm-yyyy'), 9, 8, '0.5-0.5', 'French Defense', 78); 
-INSERT INTO Games VALUES (5, 1, TO_DATE('10-03-2018', 'dd-mm-yyyy'), 3, 4, '0-1', 'Sicilian Defense', 41);
+INSERT INTO Games VALUES (1, 1, TO_DATE('10-03-2018', 'dd-mm-yyyy'), 8, 11, '1-0', 'English Opening', 62);
+INSERT INTO Games VALUES (2, 1, TO_DATE('10-03-2018', 'dd-mm-yyyy'), 14, 3, '0-1', 'Catalan Opening', 59);
+INSERT INTO Games VALUES (3, 1, TO_DATE('10-03-2018', 'dd-mm-yyyy'), 6, 4, '0.5-0.5', 'Catalan Opening', 26);
+INSERT INTO Games VALUES (4, 1, TO_DATE('10-03-2018', 'dd-mm-yyyy'), 2, 9, '1-0', 'Four Knights Game', 57); 
+INSERT INTO Games VALUES (5, 1, TO_DATE('11-03-2018', 'dd-mm-yyyy'), 11, 9, '1-0', 'Caro–Kann Defense', 32); 
+INSERT INTO Games VALUES (6, 1, TO_DATE('11-03-2018', 'dd-mm-yyyy'), 4, 2, '0.5-0.5', 'French Defense', 78); 
+INSERT INTO Games VALUES (7, 1, TO_DATE('11-03-2018', 'dd-mm-yyyy'), 3, 6, '0.5-0.5', 'Sicilian Defense', 29);
+INSERT INTO Games VALUES (8, 1, TO_DATE('11-03-2018', 'dd-mm-yyyy'), 8, 14, '0.5-0.5', 'Modern Benoni', 81);
+INSERT INTO Games VALUES (9, 1, TO_DATE('12-03-2018', 'dd-mm-yyyy'), 14, 11, '0.5-0.5', 'French Defense', 49);
+INSERT INTO Games VALUES (10, 1, TO_DATE('12-03-2018', 'dd-mm-yyyy'), 6, 8, '0-1', 'Kings Gambit', 58);
+INSERT INTO Games VALUES (11, 1, TO_DATE('12-03-2018', 'dd-mm-yyyy'), 2, 3, '0.5-0.5', 'Philidor Defense', 40);
+INSERT INTO Games VALUES (12, 1, TO_DATE('12-03-2018', 'dd-mm-yyyy'), 9, 4, '0.5-0.5', 'English Opening', 22);
+INSERT INTO Games VALUES (13, 1, TO_DATE('13-03-2018', 'dd-mm-yyyy'), 11, 4, '0.5-0.5', 'French Defense', 34);
+INSERT INTO Games VALUES (14, 1, TO_DATE('13-03-2018', 'dd-mm-yyyy'), 3, 9, '0.5-0.5', 'Queens Gambit', 76);
+INSERT INTO Games VALUES (15, 1, TO_DATE('13-03-2018', 'dd-mm-yyyy'), 8, 2, '0-1', 'Sicilian Defense', 58);
+INSERT INTO Games VALUES (16, 1, TO_DATE('13-03-2018', 'dd-mm-yyyy'), 14, 6, '0-1', 'Modern Benoni', 68);
+INSERT INTO Games VALUES (17, 1, TO_DATE('14-03-2018', 'dd-mm-yyyy'), 6, 11, '0.5-0.5', 'Center Game', 21);
+INSERT INTO Games VALUES (18, 1, TO_DATE('14-03-2018', 'dd-mm-yyyy'), 2, 14, '0.5-0.5', 'Sicilian Defense', 46);
+INSERT INTO Games VALUES (19, 1, TO_DATE('14-03-2018', 'dd-mm-yyyy'), 9, 8, '0.5-0.5', 'Kings Gambit', 31);
+INSERT INTO Games VALUES (20, 1, TO_DATE('14-03-2018', 'dd-mm-yyyy'), 4, 3, '0.5-0.5', 'Queens Gambit', 38);
--- DBMS/lab2/selects.sql
+++ DBMS/lab2/selects.sql
@@ -1,36 +1,29 @@
 --1. Найти сведения обо всех партиях, сыгранных конкретным шахматистом (имя игрока – параметр запроса);
-SELECT game_date,
-       white_player_id,
-       black_player_id,
-       RESULT,
-       debute_id,
-       turns_count,
-       tournament_id
-FROM users
-JOIN games ON ( white_player_id = users.id
-               OR black_player_id = users.id)
-WHERE first_name = 'Wilhelm'
-  AND last_name = 'Steinitz';
+SELECT Games.Id, Games.GameDate, b.LastName, w.LastName, Games.Result, Games.Debute, Games.Moves 
+FROM Games
+    LEFT JOIN Players b ON Games.WhitePlayerId = b.Id
+    LEFT JOIN Players w ON Games.BlackPlayerId = w.Id
+WHERE b.LastName = 'Caruana' OR w.LastName = 'Caruana'
 
 --2. Найти сведения обо всех шахматистах, не плативших членские взносы в течение текущего года;
-SELECT *
-FROM users
-WHERE users.id NOT IN
-    ( SELECT users.id
-     FROM users
-     JOIN membership_fees ON user_id = users.id
-     WHERE created_at >= add_months(CURRENT_DATE, -12) );
+SELECT * 
+FROM Players
+WHERE AnnualMemberPayment = 'false'
 
---3. Найти сведения о двух самых старых шахматистах, игравших в турнирах текущего года;
-SELECT *
-FROM
-  ( SELECT DISTINCT users.id,
-                    first_name,
-                    last_name,
-                    birth_date
-   FROM users
-   JOIN games ON ( white_player_id = users.id
-                  OR black_player_id = users.id )
-   WHERE (game_date >= add_months(CURRENT_DATE, -12)) )
-WHERE ROWNUM < 3
-ORDER BY birth_date;
+--3. Найти сведения о пяти самых старых шахматистах, игравших в турнирах текущего года. Включить в результат запроса сумму денег, выигранную ими на этих турнирах;
+SELECT X.*, Y.Prizes
+FROM(
+    SELECT * FROM Players
+    WHERE Id IN
+        (SELECT DISTINCT PlayerId FROM TournamentPlayers
+        WHERE TournamentId IN
+            (SELECT Id FROM Tournaments 
+            WHERE EXTRACT(YEAR from StartDate) = EXTRACT(YEAR FROM sysdate)))
+    ORDER BY BirthDate) X
+LEFT JOIN (SELECT PlayerId, SUM(Prize) AS Prizes FROM TournamentPlayers
+        WHERE TournamentId IN
+            (SELECT Id FROM Tournaments 
+            WHERE EXTRACT(YEAR from StartDate) = EXTRACT(YEAR FROM sysdate))
+        GROUP BY PlayerId) Y
+        ON Y.PlayerId = X.Id
+WHERE ROWNUM <= 5
