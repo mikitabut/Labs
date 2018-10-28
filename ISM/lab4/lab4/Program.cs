96a3f265a8215a7ad879655bf667be88e6f3c52f
PC\Andrey <minaldandrey@gmail.com>
2018-10-28 23:55:19 +0300

    ISM - Fourth lab (Implementing functionality of double integrals calculating)

--- ISM/lab4/lab4/Program.cs
+++ ISM/lab4/lab4/Program.cs
@@ -6,29 +6,47 @@ namespace lab4
 {
     public class Program
     {
+        public static Random Random = new Random();
+
         public const double FirstIntegralValue = 1.855438667260078401416698965069;
         public const double SecondIntegralValue = Math.PI / 2;
+        public const double ThirdIntegralValue = 0.550836;
 
-        delegate double Function(double x);
+        delegate double Function(double x, double y);
 
-        private static double FirstIntegralFunction(double x) => x/*Math.Exp(-Math.Pow(x, 2))*/;
+        private static double FirstIntegralFunction(double x, double y) => Math.Exp(-Math.Pow(x, 6));
+
+        private static double SecondIntegralFunction(double x, double y) 
+            => (x * x + y * y) < 1 ? -Math.Log(Math.Sqrt(x * x + y * y)) : 0;
+
+        private static double ThirdIntegralFunction(double x, double y) 
+            => (Math.Abs(x) + Math.Abs(y)) < 1 ? Math.Log(x * x + y * y + 1) : 0;
 
         public static void Main(string[] args)
         {
-            CalculateIntegral(new IntegralInfo("e^(-x^6)", FirstIntegralFunction, FirstIntegralValue, 0, 2, 0, 2));
+            CalculateIntegral(new IntegralInfo("e^(-x^6) from -infinity to infinity",
+                FirstIntegralFunction, FirstIntegralValue, 0, 1, -2, 2));
+            CalculateIntegral(new IntegralInfo("ln(1 / sqrt(x^2 + y^2)) where x^2 + y^2 < 1",
+                SecondIntegralFunction, SecondIntegralValue, 0, 4, -1, 1, -1, 1));
+            CalculateIntegral(new IntegralInfo("ln(x^2 + y^2 + 1) where |x| + |y| < 1",
+                ThirdIntegralFunction, ThirdIntegralValue, 0, 0.7, -1, 1, -1, 1));
+            Console.ReadKey();
         }
 
         private static void CalculateIntegral(IntegralInfo integralInfo)
         {
-            int count = 0;
+            int count;
             int[] iterationsAmount = { 10, 100, 1_000, 10_000, 100_000, 1_000_000, 10_000_000 };
+            Console.WriteLine(integralInfo.Info);
             foreach (int n in iterationsAmount)
             {
-                var randomX = GetUniformSequence(n, integralInfo.A, integralInfo.B);
-                var randomY = GetUniformSequence(n, integralInfo.Min, integralInfo.Max);
+                count = 0;
+                var randomX = GetUniformSequence(n, integralInfo.Ax, integralInfo.Bx);
+                var randomY = GetUniformSequence(n, integralInfo.Ay ?? 0, integralInfo.By ?? 0);
+                var randomValue = GetUniformSequence(n, integralInfo.Min, integralInfo.Max);
                 for (int i = 0; i < n; i++)
                 {
-                    if (randomY[i] < integralInfo.Function(randomX[i]))
+                    if (randomValue[i] < integralInfo.Function(randomX[i], randomY[i]))
                     {
                         count++;
                     }
@@ -36,10 +54,10 @@ namespace lab4
                 double hitPercentage = (double)count / n;
                 double monteCarloValue = integralInfo.Area * hitPercentage;
                 Console.WriteLine($"n={n}" + Environment.NewLine + 
-                    $"Real value        : {integralInfo.Value}" + Environment.NewLine +
-                    $"Monte-Carlo value : {monteCarloValue}" + Environment.NewLine);
+                    $"Real value        : {integralInfo.Value:F8}" + Environment.NewLine +
+                    $"Monte-Carlo value : {monteCarloValue:F8}");
             }
-            Console.ReadKey();
+            Console.WriteLine();
         }
 
         private static List<double> GetUniformSequence(int amount, double a, double b)
@@ -47,10 +65,9 @@ namespace lab4
             return GetRandomSequence().Select(x => a + (b - a) * x).ToList();
             IEnumerable<double> GetRandomSequence()
             {
-                var random = new Random();
                 for (int i = 0; i < amount; i++)
                 {
-                    yield return random.NextDouble();
+                    yield return Random.NextDouble();
                 }
             }
         }
@@ -62,21 +79,26 @@ namespace lab4
             public readonly double Value;
             public readonly double Min;
             public readonly double Max;
-            public readonly double A;
-            public readonly double B;
+            public readonly double Ax;
+            public readonly double Bx;
+            public readonly double? Ay;
+            public readonly double? By;
 
-            public IntegralInfo(string info, Function function, double value, double min, double max, double a, double b)
+            public IntegralInfo(string info, Function function, double value, double min, double max, 
+                double ax, double bx, double? ay = null, double? by = null)
             {
                 Info = info;
                 Function = function;
                 Value = value;
                 Min = min;
                 Max = max;
-                A = a;
-                B = b;
+                Ax = ax;
+                Bx = bx;
+                Ay = ay;
+                By = by;
             }
 
-            public double Area => (B - A) * (Max - Min);
+            public double Area => (Bx - Ax) * (Max - Min) * (Ay != null ? (By - Ay).Value : 1);
         }
     }
 }
