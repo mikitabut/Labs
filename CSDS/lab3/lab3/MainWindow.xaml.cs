6b90f711b9cf0eb8f93b2d73a33ad10fdcc74fee
Andrey Minald <minaldandrey@gmail.com>
2018-11-09 15:20:47 +0300

    CSDS - Third lab (Creating project)

---
+++ CSDS/lab3/lab3/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Win32;
+using System.IO;
+using System.Windows;
+
+namespace lab3
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CalculateSHA1_Click(object sender, RoutedEventArgs e)
+        {
+            var openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                byte[] bytes = File.ReadAllBytes(openFileDialog.FileName);
+
+
+            }
+        }
+    }
+}
