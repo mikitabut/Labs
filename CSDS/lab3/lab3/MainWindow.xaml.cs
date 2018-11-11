9b0a9543658c11030ddc04a991776b9e4e8fa22a
Andrey Minald <minaldandrey@gmail.com>
2018-11-11 23:54:25 +0300

    CSDS - Third lab (Adding SHA logic)

--- CSDS/lab3/lab3/MainWindow.xaml.cs
+++ CSDS/lab3/lab3/MainWindow.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.Win32;
+using System;
+using System.Collections;
 using System.IO;
+using System.Linq;
 using System.Windows;
 
 namespace lab3
@@ -20,8 +23,28 @@ namespace lab3
             if (openFileDialog.ShowDialog() == true)
             {
                 byte[] bytes = File.ReadAllBytes(openFileDialog.FileName);
+                bytes = FillFinishBytes(bytes);
+                //BitArray qwe =
 
+            }
+        }
 
+        private byte[] FillFinishBytes(byte[] bytes)
+        {
+            long length = bytes.LongLength;
+            var lengthInBytes = BitConverter.GetBytes(bytes.LongLength);
+            var additionalBits = GetAdditionalBits(bytes.Length % 64);
+        }
+
+        private byte[] GetAdditionalBits(int lastFragmentBytesAmount)
+        {
+            byte[] firstByte = BitConverter.GetBytes(128); //1000 0000
+            byte[] zeroByte = BitConverter.GetBytes(0);    //0000 0000
+            byte[] result;
+            if (lastFragmentBytesAmount < 56)
+            {
+                result = firstByte;
+                result.Concat(Enumerable.Repeat(zeroByte, 2).ToArray());
             }
         }
     }
