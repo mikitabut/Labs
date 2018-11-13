2118bdfe465ac54317494ac3a44676f4813122f1
S.W.A.T.  O.  P.L.U.S <swatoplus@gmail.com>
2018-11-13 05:27:37 +0300

    Fixed Classes window

--- NLP/NLP/ClassesWindow.xaml.cs
+++ NLP/NLP/ClassesWindow.xaml.cs
@@ -25,10 +25,40 @@ namespace NLP
         }
 
         private int _picNumber = 0;
+        private const int PicCount = 7;
 
         private void Window_Initialized(object sender, EventArgs e)
         {
+            RefreshImage();
+        }
+
+        private void RefreshImage()
+        {
             MainImage.Source = new BitmapImage(new Uri($"/resources/classes{_picNumber}.png", UriKind.Relative));
+        }
+
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            _picNumber--;
+
+            if (_picNumber == 0)
+            {
+                _picNumber = PicCount - 1;
+            }
+
+            RefreshImage();
+        }
+
+        private void NextButton_Click(object sender, RoutedEventArgs e)
+        {
+            _picNumber++;
+
+            if (_picNumber == PicCount)
+            {
+                _picNumber = 0;
+            }
+
+            RefreshImage();
         }
     }
 }
